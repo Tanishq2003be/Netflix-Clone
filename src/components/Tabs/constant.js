b50085a748bfb51a165ee9e36a6e7432f78a7a8c
Tanishq Dublish <tdublish_be21@thapar.edu>
2023-05-30 14:22:21 +0530

    alpha

---
+++ src/components/Tabs/constant.js
@@ -0,0 +1,5 @@
+export const tabLabels = {
+    CANCEL_AT_ANY_TIME: "Cancel at any time",
+    WATCH_ANWHERE: "Watch Anywhere",
+    PICK_YOUR_PRICE: "Pick Your Price",
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+ import React from 'react'
+import Header1 from '../components/header/Header1'
+import TabComponent from '../components/Tabs'
+ const Home = () => {
+   return (
+    <>
+    <Header1/>
+    <TabComponent/>
+    </>
+   )
+ }
+ 
+ export default Home
+ 
